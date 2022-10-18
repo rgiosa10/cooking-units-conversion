66fd2d45107acf2dcb338daa77838ece7332d627
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-18 10:13:38 -0700

    Added pounds to ounces to grams calc and reordered prompts and alerts
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
+//Ounces input
+
 const oz = parseFloat(prompt("Please input number of ounces (oz): "))
-// const lbs = parseFloat(prompt("Please input number of pounds (lbs): "))
 
 function ouncesToPoundsConv(oz) {
   const results = oz * .0625;
   return results;
 }
 
-// function poundsToOuncesConv(lbs) {
-//   const results = lbs / .0625;
-//   return results;
-// }
-
 ouncesToPoundsResult = ouncesToPoundsConv(oz);
-// poundsToOuncesResult = poundToOuncesConv(lbs);
-
 poundsToGramsResult = 454 * ouncesToPoundsResult;
-// ouncesToGramsResult = 28.3 * poundsToOuncesResult;
 
 window.alert(oz + " ounces are equal to " + ouncesToPoundsResult + "pounds and " + poundsToGramsResult + " grams");
-// window.alert(lbs + " pounds are equal to " + poundsToOuncesResult + "ounces and " + ouncesToGramsResult + " grams");
+
+//Pounds input
+
+const lbs = parseFloat(prompt("Please input number of pounds (lbs): "))
+
+function poundsToOuncesConv(lbs) {
+  const results = lbs / .0625;
+    return results;
+}
+
+poundsToOuncesResult = poundsToOuncesConv(lbs);
+ouncesToGramsResult = 28.3 * poundsToOuncesResult;
+
+window.alert(lbs + " pounds are equal to " + poundsToOuncesResult + " ounces and " + ouncesToGramsResult + " grams");
